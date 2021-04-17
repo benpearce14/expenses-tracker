6f0b96e23297efc32a21631e4e6e3d27b32256e8
Ben Pearce <b.j.pearce@durham.ac.uk>
2021-04-17 20:13:01 +0100

    Started Expenses Components

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,17 @@
+import "./ExpenseItem.css";
+import Card from "./Card";
+import ExpenseDate from "./ExpenseDate.js";
+
+const ExpenseItem = (props) => {
+	return (
+		<Card className='expense-item'>
+			<ExpenseDate date={props.date} />
+			<div className='expense-item__description'>
+				<h2>{props.title}</h2>
+				<div className='expense-item__price'>£{props.amount}</div>
+			</div>
+		</Card>
+	);
+};
+
+export default ExpenseItem;
